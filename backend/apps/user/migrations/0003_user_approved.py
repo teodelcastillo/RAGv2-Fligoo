53d7828015ea8bde392826a807f671f1e24fb65c
teodelcastillo <teodorodelcastillop@gmail.com>
2026-04-04 20:22:58 -0300

    user approved migration uses IF NOT EXISTS

--- backend/apps/user/migrations/0003_user_approved.py
+++ backend/apps/user/migrations/0003_user_approved.py
@@ -8,9 +8,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="user",
-            name="approved",
-            field=models.BooleanField(default=False),
+        migrations.RunSQL(
+            sql="ALTER TABLE user_user ADD COLUMN IF NOT EXISTS approved boolean NOT NULL DEFAULT false;",
+            reverse_sql="ALTER TABLE user_user DROP COLUMN IF EXISTS approved;",
+            state_operations=[
+                migrations.AddField(
+                    model_name="user",
+                    name="approved",
+                    field=models.BooleanField(default=False),
+                ),
+            ],
         ),
     ]

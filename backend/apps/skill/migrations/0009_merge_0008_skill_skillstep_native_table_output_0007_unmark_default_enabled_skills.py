583a65cfbc94123c20afd8df56e6b2772477b504
Teodoro del Castillo <teodorodelcastillop@gmail.com>
2026-05-07 13:00:31 -0300

    merge migrations

---
+++ backend/apps/skill/migrations/0009_merge_0008_skill_skillstep_native_table_output_0007_unmark_default_enabled_skills.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("skill", "0008_skill_skillstep_native_table_output"),
+        ("skill", "0007_unmark_default_enabled_skills"),
+    ]
+
+    operations = []
